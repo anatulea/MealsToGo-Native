f541fcd9b4c7c464045d33d1bf756297c1e40113
Ana Tulea <anatulea14@gmail.com>
2021-05-11 19:28:33 -0700

    addes styled components

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { Text } from "react-native";
-import { Card, Title } from "react-native-paper";
+import styled from "styled-components/native";
+import { Card } from "react-native-paper";
 
-export const RestaurantInfo = ({ restaurant = {} }) => {
+const Title = styled.Text`
+  padding: 16px;
+  color: red;
+`;
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
-    name = "test name",
+    name = "Test name",
     icon,
     photos = [
       "https://images.unsplash.com/photo-1493770348161-369560ae357d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
