f5401e04046c50ac41d03d4005330f5df6d1a6a9
Ana Tulea <anatulea14@gmail.com>
2021-05-12 18:43:49 -0700

    created restaurants flat list

--- src/features/restaurants/components/restaurant-info-card.styles.js
+++ src/features/restaurants/components/restaurant-info-card.styles.js
@@ -2,11 +2,12 @@ import styled from "styled-components/native";
 import { Card } from "react-native-paper";
 
 export const RestaurantCard = styled(Card)`
-  background-color: ${(props) => props.theme.colors.ui.quaternary};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+  margin-bottom: ${(props) => props.theme.space[3]};
 `;
 
 export const RestaurantCardCover = styled(Card.Cover)`
-  background-color: ${(props) => props.theme.colors.ui.tertiary};
+  background-color: ${(props) => props.theme.colors.bg.primary};
   padding: ${(props) => props.theme.space[3]};
 `;
 
