742ed65a6306748c6fce62d1532d7d28a9660393
Ana Tulea <anatulea14@gmail.com>
2021-05-20 13:13:50 -0700

    tested autheniticaton

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
 
@@ -15,11 +15,43 @@ import { LocationContextProvider } from "./src/services/location/location.contex
 import { Navigation } from "./src/infrastructure/navigation/index";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
 
+import * as firebase from "firebase";
+// require("firebase/auth");
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDQm8uMLfU0iuC0alRCYDiHYdicZI2QwxM",
+  authDomain: "mealstogo-22cfe.firebaseapp.com",
+  projectId: "mealstogo-22cfe",
+  storageBucket: "mealstogo-22cfe.appspot.com",
+  messagingSenderId: "854021424475",
+  appId: "1:854021424475:web:085c5cd04917c73ef6867e",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
 export default function App() {
-  let [oswaldLoaded] = useOswald({
+  const [isAuthenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    firebase.default
+      .auth()
+      .signInWithEmailAndPassword("anatulea@email.com", "anatulea")
+      .then((user) => {
+        console.log(`user`, user);
+        setAuthenticated(true);
+      })
+      .catch((e) => {
+        console.log(`error from firebase!!!!!!`, e);
+      });
+  }, []);
+
+  const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
-  let [latoLoaded] = useLato({
+  const [latoLoaded] = useLato({
     Lato_400Regular,
   });
 
