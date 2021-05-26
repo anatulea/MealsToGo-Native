6c793cef25a78d6d56d0ebfa3ba6da27d2e3eefc
Ana Tulea <anatulea14@gmail.com>
2021-05-25 17:35:43 -0700

    Moving Restaurants Mock

---
+++ functions/places/index.js
@@ -0,0 +1,9 @@
+const { mocks, addMockImage } = require("./mock");
+module.exports.placesRequest = (request, response) => {
+  const location = request.query.location;
+  const data = mocks[location];
+  if (data) {
+    data.results = data.results.map(addMockImage);
+  }
+  response.json(data);
+};
--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,21 +1,16 @@
-import { mockImages, mocks } from "./mock";
+// import { mockImages, mocks } from "./mock";
 import camelize from "camelize";
 
 export const restaurantsRequest = (location) => {
-  return new Promise((resolve, reject) => {
-    const mock = mocks[location];
-    if (!mock) {
-      reject("not, found");
-    }
-    resolve(mock);
+  return fetch(
+    `http://localhost:5001/mealstogo-22cfe/us-central1/placesNearby?location=${location}`
+  ).then((res) => {
+    return res.json();
   });
 };
 
 export const restaurantsTransform = ({ results = [] }) => {
   const mappedResults = results.map((restaurant) => {
-    restaurant.photos = restaurant.photos.map((p) => {
-      return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))];
-    });
     return {
       ...restaurant,
       address: restaurant.vicinity,
