b58455c0df6aff199dfe9cdc1377a19573ca4682
Ana Tulea <anatulea14@gmail.com>
2021-05-26 16:59:05 -0700

    Setting Up Google Node SDK

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,10 +1,29 @@
 const { locations: locationsMock } = require("./geocode.mock");
-
+const url = require("url");
 const functions = require("firebase-functions");
 
-module.exports.geocodeRequest = (request, response) => {
-  const city = request.query.city;
+module.exports.geocodeRequest = (request, response, client) => {
+  // const city = request.query.city;
+  const { city, mock } = url.parse(request.url, true).query;
 
-  const locationMock = locationsMock[city.toLowerCase()];
-  response.json(locationMock);
+  // if (locationsMock[city] !== undefined) {
+  if (mock === "true") {
+    const locationMock = locationsMock[city.toLowerCase()];
+    response.json(locationMock);
+  }
+  client
+    .geocode({
+      params: {
+        address: city,
+        key: functions.config().google.key,
+      },
+      timeout: 1000,
+    })
+    .then((res) => {
+      return response.json(res.data);
+    })
+    .catch((e) => {
+      response.status(400);
+      return response.send(e.response.data.error_message);
+    });
 };
