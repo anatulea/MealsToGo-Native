9d79472ebacdc62d37e36ebffe2977f0ad2d1ca0
Ana Tulea <anatulea14@gmail.com>
2021-05-26 17:42:24 -0700

    minor optimizations

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,11 +1,13 @@
 // import { mockImages, mocks } from "./mock";
 import camelize from "camelize";
-import { host } from "../../utils/env.js";
+import { host, isMock } from "../../utils/env.js";
 
 export const restaurantsRequest = (location) => {
-  return fetch(`${host}/placesNearby?location=${location}`).then((res) => {
-    return res.json();
-  });
+  return fetch(`${host}/placesNearby?location=${location}&mock=${isMock}`).then(
+    (res) => {
+      return res.json();
+    }
+  );
 };
 
 export const restaurantsTransform = ({ results = [] }) => {
