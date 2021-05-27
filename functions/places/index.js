df56a8aa93ce4c5283fecb8e0189fa0d42d5d3d4
Ana Tulea <anatulea14@gmail.com>
2021-05-26 17:23:17 -0700

    Integrating Places API

--- functions/places/index.js
+++ functions/places/index.js
@@ -1,9 +1,34 @@
 const { mocks, addMockImage } = require("./mock");
-module.exports.placesRequest = (request, response) => {
-  const location = request.query.location;
-  const data = mocks[location];
-  if (data) {
-    data.results = data.results.map(addMockImage);
+const url = require("url");
+const functions = require("firebase-functions");
+
+module.exports.placesRequest = (request, response, client) => {
+  // const location = request.query.location;
+  const { location, mock } = url.parse(request.url, true).query;
+  if (mock === "true") {
+    const data = mocks[location];
+    if (data) {
+      data.results = data.results.map(addMockImage);
+    }
+
+    return response.json(data);
   }
-  response.json(data);
+  client
+    .placesNearby({
+      params: {
+        location: location,
+        radius: 1500,
+        type: "restaurant",
+        key: functions.config().google.key,
+      },
+      timeout: 1000,
+    })
+    .then((res) => {
+      res.data.results = res.data.results.map(addMockImage);
+      return response.json(res.data);
+    })
+    .catch((e) => {
+      response.status(400);
+      return response.send(e.response.data.error_message);
+    });
 };
